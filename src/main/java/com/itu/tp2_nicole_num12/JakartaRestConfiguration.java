83d482f810f97e4c38ce08ad4070651c7a298b1a
Nic Rah <nicrah16@gmail.com>
2022-04-03 23:46:25 +0300

    prg avec jsf

---
+++ src/main/java/com/itu/tp2_nicole_num12/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.itu.tp2_nicole_num12;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/itu/tp2_nicole_num12/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itu.tp2_nicole_num12.config;
+
+/**
+ *
+ * @author Nicole
+ */
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
